a689861e8dd836efb1b7f84c9ca3f7adfe840000
Tim Thompson <scubatim84@gmail.com>
2020-10-03 08:59:29 -0600

    Add expo-file-system package to project, add logic to store/actions/places.ts to move file from temporary memory to permanent memory location

--- store/actions/places.ts
+++ store/actions/places.ts
@@ -1,5 +1,28 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title: string, imageUrl: string) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: imageUrl } };
+  return async (dispatch: any) => {
+    const fileName: string | undefined = imageUrl.split('/').pop();
+    const folderPath: string | null = FileSystem.documentDirectory;
+
+    let newPath;
+
+    if (fileName && folderPath) {
+      newPath = folderPath + fileName;
+
+      try {
+        await FileSystem.moveAsync({
+          from: imageUrl,
+          to: newPath,
+        });
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
